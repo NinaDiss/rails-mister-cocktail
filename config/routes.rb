f4a1908d0583b6dc17de6e6b93fed9f237e8caba
Nina Dissard <dissardnina@gmail.com>
2020-11-12 14:55:00 +0100

    Create a route, a CocktailsController and an index view to display all cocktails.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # A user can see the list of cocktails
+  get "/cocktails", to: "cocktails#index"
+
+
 end
