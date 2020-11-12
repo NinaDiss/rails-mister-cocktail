809fabf192e3773a4ed8014a3f16d6c3fbcb4187
Nina Dissard <dissardnina@gmail.com>
2020-11-12 12:45:43 +0100

    Create dependance relations between models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  # Association unique entre le cocktail et ses ingredients
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
